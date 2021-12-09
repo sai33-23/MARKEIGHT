76fb58efadceb2443fdc5cfe4f19fdc1c3a6ad1f
sai33-23 <84378922+sai33-23@users.noreply.github.com>
2021-12-09 15:00:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+var emojiDictionary = {
+  "😊": "Smiling",
+  "😳": "disbelief",
+  "😔": "sad",
+  "🥡": "takeout box",
+  "❤️": "love",
+  "😑": "annoyance",
+  "🤪": "Zany Face",
+  "😴": "Sleeping Face",
+  "🐕": "Dog",
+  " 🇨🇦": "Canada",
+  "🇮🇳": "India"
+};
+
+var emojiWeknow = Object.keys(emojiDictionary);
+// console.log(emojiWeknow);
+// for (i=0;i<emojiWeknow.length;i++){
+//   console.log(emojiDictionary[emojiWeknow[i]])
+// }
+
+export default function App() {
+  const [meaning, setmeaning] = useState(" ");
+  function emojiInputHandler(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    console.log(userInput + " " + meaning);
+
+    if (meaning === undefined) {
+      meaning = "we dont have this in our database";
+    }
+    setmeaning(meaning);
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setmeaning(meaning);
+  }
+  return (
+    <div className="App">
+      <h1>Emoji Pedia</h1>
+      <input
+        onChange={emojiInputHandler}
+        placeholder={"search your emoji🔍"}
+        style={{
+          padding: "0.7rem 10rem 0.7rem 0.5rem",
+          fontSize: "1.5rem",
+          borderRadius: "0.5rem",
+          borderColor: "skyblue",
+          backgroundColor: "wheat"
+        }}
+      />
+      <h2> {meaning}</h2>
+      <h3> emoji We know</h3>
+      {emojiWeknow.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.7rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
